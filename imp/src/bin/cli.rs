1cbcb6aea92ab93ec67c362da1010211522646b5
Russel Arbore <russel.jma@gmail.com>
2025-10-09 19:44:48 -0700

    Bare bones dot generation

--- imp/src/bin/cli.rs
+++ imp/src/bin/cli.rs
@@ -1,8 +1,28 @@
 use std::io::{Read, Result, stdin};
+use std::process::Command;
 
+use tempfile::NamedTempFile;
+
+use imp::ast::Interner;
 use imp::grammar::ProgramParser;
 use imp::term::{naive_ssa_translation, terms_to_dot};
 
 pub fn main() -> Result<()> {
+    let mut interner = Interner::new();
+
+    let mut imp_program = String::new();
+    stdin().read_to_string(&mut imp_program)?;
+    let mut location = 0;
+    let ast = ProgramParser::new()
+        .parse(&mut interner, &mut location, &imp_program)
+        .unwrap();
+
+    for func in &ast.funcs {
+        let terms = naive_ssa_translation(func);
+        let mut tmp = NamedTempFile::new().unwrap();
+        terms_to_dot(&terms, &mut tmp)?;
+        Command::new("xdot").arg(tmp.path()).status().unwrap();
+    }
+
     Ok(())
 }
--- imp/src/term.rs
+++ imp/src/term.rs
@@ -1,4 +1,8 @@
 use std::collections::BTreeMap;
+use std::io::{Result, Write};
+
+use dot::{Edges, GraphWalk, Id, Labeller, Nodes, render};
+use string_interner::Symbol as _;
 
 use crate::ast::{ExpressionAST, FunctionAST, Location, StatementAST, Symbol};
 
@@ -34,36 +38,47 @@ pub enum BinaryOp {
     GE,
 }
 
-pub type Terms = (Vec<Term>, BTreeMap<Term, TermId>);
-
 type Context = BTreeMap<Symbol, TermId>;
 
-fn add_term(terms: &mut Terms, term: Term) -> TermId {
-    if let Some(id) = terms.1.get(&term) {
-        *id
-    } else {
-        let id = alloc_term(terms);
-        set_term(terms, id, term);
+#[derive(Debug, Clone)]
+pub struct Terms {
+    name: Symbol,
+    terms: Vec<Term>,
+    intern: BTreeMap<Term, TermId>,
+}
+
+impl Terms {
+    fn add_term(&mut self, term: Term) -> TermId {
+        if let Some(id) = self.intern.get(&term) {
+            *id
+        } else {
+            let id = self.alloc_term();
+            self.set_term(id, term);
+            id
+        }
+    }
+
+    fn alloc_term(&mut self) -> TermId {
+        let id = self.terms.len() as TermId;
+        self.terms.push(Term::Param(!0));
         id
+    }
+
+    fn set_term(&mut self, id: TermId, term: Term) {
+        self.terms[id as usize] = term;
+        self.intern.insert(term, id);
     }
 }
 
-fn alloc_term(terms: &mut Terms) -> TermId {
-    let id = terms.0.len() as TermId;
-    terms.0.push(Term::Param(!0));
-    id
-}
-
-fn set_term(terms: &mut Terms, id: TermId, term: Term) {
-    terms.0[id as usize] = term;
-    terms.1.insert(term, id);
-}
-
 pub fn naive_ssa_translation(func: &FunctionAST) -> Terms {
-    let mut terms = Terms::default();
+    let mut terms = Terms {
+        name: func.name,
+        terms: vec![],
+        intern: BTreeMap::new(),
+    };
     let mut ctx = Context::new();
     for (idx, sym) in func.params.iter().enumerate() {
-        ctx.insert(*sym, add_term(&mut terms, Term::Param(idx as i32)));
+        ctx.insert(*sym, terms.add_term(Term::Param(idx as i32)));
     }
     handle_stmt(&mut ctx, &mut terms, &func.body);
     terms
@@ -72,9 +87,7 @@ pub fn naive_ssa_translation(func: &FunctionAST) -> Terms {
 fn handle_stmt(ctx: &mut Context, terms: &mut Terms, stmt: &StatementAST) {
     use StatementAST::*;
     match stmt {
-        Block(_, stmts) => {
-            stmts.iter().for_each(|stmt| handle_stmt(ctx, terms, stmt))
-        }
+        Block(_, stmts) => stmts.iter().for_each(|stmt| handle_stmt(ctx, terms, stmt)),
         Assign(_, sym, expr) => {
             let term = handle_expr(ctx, terms, expr);
             ctx.insert(*sym, term);
@@ -88,15 +101,20 @@ fn handle_stmt(ctx: &mut Context, terms: &mut Terms, stmt: &StatementAST) {
                 handle_stmt(&mut else_ctx, terms, else_stmt);
             }
             for (sym, then_term) in &then_ctx {
-                let Some(else_term) = else_ctx.get(sym) else { continue };
-                ctx.insert(*sym, add_term(terms, Term::Phi(*loc, *then_term, *else_term)));
+                let Some(else_term) = else_ctx.get(sym) else {
+                    continue;
+                };
+                ctx.insert(
+                    *sym,
+                    terms.add_term(Term::Phi(*loc, *then_term, *else_term)),
+                );
             }
         }
         While(loc, cond_expr, body_stmt) => {
             let mut sym_entry_phi_tuples = vec![];
             for (sym, entry) in ctx.iter_mut() {
                 let old_entry = *entry;
-                let phi = alloc_term(terms);
+                let phi = terms.alloc_term();
                 *entry = phi;
                 sym_entry_phi_tuples.push((*sym, old_entry, phi));
             }
@@ -105,79 +123,122 @@ fn handle_stmt(ctx: &mut Context, terms: &mut Terms, stmt: &StatementAST) {
             handle_stmt(&mut body_ctx, terms, body_stmt);
             for (sym, entry, phi) in sym_entry_phi_tuples {
                 let bottom = body_ctx[&sym];
-                set_term(terms, phi, Term::Phi(*loc, entry, bottom));
+                terms.set_term(phi, Term::Phi(*loc, entry, bottom));
             }
         }
         Return(_, expr) => {
             handle_expr(ctx, terms, expr);
-        },
+        }
     }
 }
 
 fn handle_expr(ctx: &Context, terms: &mut Terms, expr: &ExpressionAST) -> TermId {
     use ExpressionAST::*;
     match expr {
-        NumberLiteral(val) => add_term(terms, Term::Constant(*val)),
+        NumberLiteral(val) => terms.add_term(Term::Constant(*val)),
         Variable(sym) => ctx[sym],
         Call(_, _) => todo!(),
         Add(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::Add, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::Add, lhs, rhs))
         }
         Subtract(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::Sub, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::Sub, lhs, rhs))
         }
         Multiply(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::Mul, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::Mul, lhs, rhs))
         }
         Divide(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::Div, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::Div, lhs, rhs))
         }
         Modulo(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::Rem, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::Rem, lhs, rhs))
         }
         EqualsEquals(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::EE, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::EE, lhs, rhs))
         }
         NotEquals(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::NE, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::NE, lhs, rhs))
         }
         Less(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::LT, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::LT, lhs, rhs))
         }
         LessEquals(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::LE, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::LE, lhs, rhs))
         }
         Greater(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::GT, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::GT, lhs, rhs))
         }
         GreaterEquals(lhs, rhs) => {
             let lhs = handle_expr(ctx, terms, lhs);
             let rhs = handle_expr(ctx, terms, rhs);
-            add_term(terms, Term::Binary(BinaryOp::GE, lhs, rhs))
+            terms.add_term(Term::Binary(BinaryOp::GE, lhs, rhs))
         }
     }
 }
 
-pub fn terms_to_dot(terms: &Terms) {
+pub fn terms_to_dot<W: Write>(terms: &Terms, w: &mut W) -> Result<()> {
+    render(terms, w)
+}
 
+impl<'a> GraphWalk<'a, (TermId, Term), (TermId, TermId)> for Terms {
+    fn nodes(&'a self) -> Nodes<'a, (TermId, Term)> {
+        self.terms
+            .iter()
+            .enumerate()
+            .map(|(idx, term)| (idx as TermId, *term))
+            .collect()
+    }
+
+    fn edges(&'a self) -> Edges<'a, (TermId, TermId)> {
+        let mut edges = vec![];
+        for (idx, term) in self.terms.iter().enumerate() {
+            match term {
+                Term::Constant(_) | Term::Param(_) => {}
+                Term::Unary(_, dst) => edges.push((idx as TermId, *dst)),
+                Term::Phi(_, dst1, dst2) | Term::Binary(_, dst1, dst2) => {
+                    edges.push((idx as TermId, *dst1));
+                    edges.push((idx as TermId, *dst2));
+                }
+            }
+        }
+        edges.into()
+    }
+
+    fn source(&'a self, edge: &(TermId, TermId)) -> (TermId, Term) {
+        (edge.0, self.terms[edge.0 as usize])
+    }
+
+    fn target(&'a self, edge: &(TermId, TermId)) -> (TermId, Term) {
+        (edge.1, self.terms[edge.1 as usize])
+    }
+}
+
+impl<'a> Labeller<'a, (TermId, Term), (TermId, TermId)> for Terms {
+    fn graph_id(&'a self) -> Id<'a> {
+        Id::new(format!("F{}", self.name.to_usize())).unwrap()
+    }
+
+    fn node_id(&'a self, n: &(TermId, Term)) -> Id<'a> {
+        Id::new(format!("N{}", n.0)).unwrap()
+    }
 }
